c7553582e84989d93d859fd348a290bfc1c1740c
DuckTail <154976490+TrDuckTail@users.noreply.github.com>
2024-09-07 19:11:44 +0700

    Update install-docker-on-ubuntu-22.04.sh

--- install-docker-on-ubuntu-22.04.sh
+++ install-docker-on-ubuntu-22.04.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
 echo "=== Install Docker Community Edition on Ubuntu 22.04 ==="
+echo
 
 echo "=== Preparation Before Installation ==="
-echo
-
 sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
+# Set up the Docker stable repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Update the package list to include Docker’s packages
 sudo apt update
 
+# Verify Docker CE is available in the repository
 sudo apt-cache policy docker-ce
 
-echo "=== Installing Docker Community Edition on Ubuntu 22.04 ==="
-echo
-
-sudo apt install docker-ce
+echo "=== Installing Docker Community Edition ==="
+sudo apt install -y docker-ce
 
 echo
-echo "=== Finish Installing ==="
+echo "=== Docker Installation Completed ==="
 echo
 
-echo "=== Checking status docker ==="
+# Check Docker status
+echo "=== Checking Docker status ==="
 sudo systemctl status docker
-
-
-
-
